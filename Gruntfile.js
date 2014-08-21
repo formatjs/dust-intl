04c2534399568fff87e9a387fe92d17e49ca17d6
Eric Ferraiuolo <eferraiuolo@gmail.com>
2014-08-20 17:09:46 -0700

    Refactor Grunt build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,34 +3,56 @@ module.exports = function (grunt) {
     var libpath = require('path');
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            dist: 'dist/',
+            lib : 'lib/',
+            tmp : 'tmp/'
+        },
+
+        copy: {
+            tmp: {
+                expand : true,
+                flatten: true,
+                src    : 'tmp/src/*.js',
+                dest   : 'lib/'
+            }
+        },
+
         jshint: {
             all: ['src/*.js', 'tests/*.js']
         },
+
         bundle_jsnext: {
+            dest: 'dist/dust-intl.js',
+
             options: {
-                namespace: 'DustHelperIntl'
-            },
-            dest: 'dist/helpers.js'
+                namespace: 'DustIntl'
+            }
         },
+
         cjs_jsnext: {
-            dest: 'lib/'
+            dest: 'tmp/'
         },
+
         uglify: {
             options: {
                 preserveComments: 'some'
             },
+
             dist: {
-                src: 'dist/helpers.js',
-                dest: 'dist/helpers.min.js'
+                src : 'dist/dust-intl.js',
+                dest: 'dist/dust-intl.min.js'
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-bundle-jsnext-lib');
 
-    grunt.registerTask('build', ['bundle_jsnext', 'cjs_jsnext', 'uglify:dist']);
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', [
+        'jshint', 'clean', 'bundle_jsnext', 'uglify', 'cjs_jsnext', 'copy'
+    ]);
 };
