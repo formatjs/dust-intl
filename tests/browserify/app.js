49e1985c3eb79b33613117d25f816127f1c50875
Eric Ferraiuolo <edf@ericf.me>
2015-03-23 15:46:49 -0400

    Change how Browserify tests are loaded in the test page

--- tests/browserify/app.js
+++ tests/browserify/app.js
@@ -1,11 +1,5 @@
 /* global dust */
 'use strict';
 
-var DustIntl = require('../../');
+global.DustIntl = require('../../');
 require('../../lib/locales.js');
-
-// Load dust with its compiler.
-global.dust = require('dustjs-linkedin/lib/server.js');
-DustIntl.registerWith(dust);
-
-require('../helpers.js');
